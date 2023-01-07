1ea4765ae96ba90f84169d75fcac2cf45e6bebb0
sqcodeworks <sqcodeworks@gmail.com>
2023-01-07 18:14:51 +0500

    Update Hello.js

--- src/Hello.js
+++ src/Hello.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './Hello.css';
 
 function Hello({name}) {
-  return <p className='myname'>This is a paragraph in Hello Tag. <br/>Name = {name}</p>
+  return <p className='myname'>Hello From Hello Tag <br/>Name = {name}</p>
 }
 
 export default Hello;
